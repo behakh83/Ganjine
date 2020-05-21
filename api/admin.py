c73ec31b1e564b248e85e305f5e38b9221d9e10c
behakh <behakh83@gmail.com>
2020-05-21 23:42:47 +0430

    add Collection and Question model to admin panel.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+# the module name is app_name.models
+from api.models import Collection, Question
+# Register your models to admin site, then you can add, edit, delete and search your models in Django admin site.
+admin.site.register(Collection)
+admin.site.register(Question)
