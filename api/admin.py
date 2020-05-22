c16cba9fae71361ff6e8beee00c45a914ca48b85
behakh <behakh83@gmail.com>
2020-05-22 06:56:02 +0430

    Add TabularInline ability to manage questions inside Collections admin panel.

--- api/admin.py
+++ api/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
+from django.contrib.contenttypes.admin import GenericTabularInline
 from rest_framework.authtoken.models import Token
-
 from api.models import Collection, Question
 
-admin.site.register(Collection)
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+
+class CollectionAdmin(admin.ModelAdmin):
+    inlines = [
+        QuestionInline,
+    ]
+
+
+admin.site.register(Collection, CollectionAdmin)
 admin.site.register(Question)
 
 admin.site.unregister(User)
